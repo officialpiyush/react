b9019e0a7271550cc5d1cca6d6e42399343bc377
Piyush <bhangalepiyush@gmail.com>
2022-12-25 23:41:51 +0530

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Suspense } from "react"
+import { useRoutes } from "react-router"
+import routes from "~react-pages"
+
 function App() {
   return (
-   <div></div>
+    <div>
+      <Suspense fallback={<div>Loading</div>}>
+        {useRoutes(routes)}
+      </Suspense>
+    </div>
   )
 }
 
